67cb5ef108256004d4f70d1fd513eabc98a805af
cat-rivers <catalina.riveros.m@gmail.com>
2019-07-27 08:33:29 +0300

    cookedporridgep age

--- src/App.js
+++ src/App.js
@@ -2,36 +2,58 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  const [page, setPage] = React.useState("mainpage");
+
+  const variable = "Hello cati";
+
+  function doMeOnClick() {
+    if (page === "mainpage") {
+      setPage("cooked-porridge");
+    } else {
+      setPage("mainpage");
+    }
+  }
+
+  const PageComponent = () =>
+    page === "mainpage" ? <MainPage /> : <CookedPorridge />;
+
   return (
     <div className="content">
-      react porridge
-      <h1>Hello World</h1>
-      <h2>I write about Porridge</h2>
-      <p>
-        So Porridge is a topic ive always been interested in. I think not enough
-        people are interested in porridge. There are different ways to eat it
-        and each country has their own variation of it
-      </p>
-      <div className="lists">
-        <ul>
-          <li>
-            <a href="cooked porridge.html">Cooked Porridge</a>
-          </li>
-          <li>Cold Porridge</li>
-          <li>Toni Porridge</li>
-          <li>Crazy Porridge</li>
-        </ul>
-      </div>
-      <div>
-        <p id="footer"> Cat Rivers on Porrdige (TM)</p>
-      </div>
+      {page}
+      <button onClick={doMeOnClick}>Change page</button>
+      <PageComponent />
     </div>
   );
 }
 
 export default App;
 
-const PorridgeOne = () => (
+const MainPage = () => (
+  <>
+    <h1>Hello World</h1>
+    <h2>I write about Porridge</h2>
+    <p>
+      So Porridge is a topic ive always been interested in. I think not enough
+      people are interested in porridge. There are different ways to eat it and
+      each country has their own variation of it
+    </p>
+    <div className="lists">
+      <ul>
+        <li>
+          <button>Cooked Porridge</button>
+        </li>
+        <li>Cold Porridge</li>
+        <li>Toni Porridge</li>
+        <li>Crazy Porridge</li>
+      </ul>
+    </div>
+    <div>
+      <p id="footer"> Cat Rivers on Porrdige (TM)</p>
+    </div>
+  </>
+);
+
+const CookedPorridge = () => (
   <div className="content">
     <div className="title">
       <h1> Hi! I love cooked Porridge!</h1>
